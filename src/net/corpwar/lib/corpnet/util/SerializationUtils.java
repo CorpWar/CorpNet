1fbe906dc6e1e04b78a51f73772de16c961688ce
Ghost <ghostnr1@gmail.com>
2014-12-19 22:02:06 +0100

    Have fixed some stuff from SonarQube

--- src/net/corpwar/lib/corpnet/util/SerializationUtils.java
+++ src/net/corpwar/lib/corpnet/util/SerializationUtils.java
@@ -46,19 +46,19 @@ public class SerializationUtils {
             oos.writeObject(obj);
             oos.reset();
         } catch (IOException e) {
-            LOG.log(Level.FINE, "Error IOException");
+            LOG.log(Level.FINE, "Error IOException", e);
         } finally {
             if (oos != null) {
                 try {
                     oos.close();
                 } catch (IOException e) {
-                    LOG.log(Level.FINE, "Error IOException");
+                    LOG.log(Level.FINE, "Error IOException", e);
                 }
             }
             try {
                 baos.close();
             } catch (IOException e) {
-                LOG.log(Level.FINE, "Error IOException");
+                LOG.log(Level.FINE, "Error IOException", e);
                 }
         }
         byte[] byteObj = baos.toByteArray();
@@ -76,23 +76,23 @@ public class SerializationUtils {
             ois = new ObjectInputStream(bais);
             obj = (T) ois.readObject();
         } catch (IOException e) {
-            LOG.log(Level.FINE, "Error IOException");
+            LOG.log(Level.FINE, "Error IOException", e);
         } catch (ClassNotFoundException e) {
-            LOG.log(Level.FINE, "Error Class not found");
+            LOG.log(Level.FINE, "Error Class not found", e);
             return null;
         } finally {
             if (bais != null) {
                 try {
                     bais.close();
                 } catch (IOException e) {
-                    LOG.log(Level.FINE, "Error IOException");
+                    LOG.log(Level.FINE, "Error IOException", e);
                 }
             }
             if (ois != null) {
                 try {
                     ois.close();
                 } catch (IOException e) {
-                    LOG.log(Level.FINE, "Error IOException");
+                    LOG.log(Level.FINE, "Error IOException", e);
                 }
             }
         }
